b28f6da7155f5f2518d7e6816aabd616502a9c0d
Leon Galushko <leon-galushko@mail.ru>
2020-07-31 18:54:30 +0300

    logging for controller

--- src/main/java/ru/graduation/controller/UserController.java
+++ src/main/java/ru/graduation/controller/UserController.java
@@ -1,5 +1,7 @@
 package ru.graduation.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.Assert;
 import ru.graduation.model.User;
@@ -12,30 +14,36 @@ import static ru.graduation.util.ValidationUtil.checkNotFoundWithId;
 @Controller
 public class UserController {
     private final UserRepository repository;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     public UserController(UserRepository repository) {
         this.repository = repository;
     }
 
     public List<User> getAll() {
+        logger.info("getAll users");
         return repository.getAll();
     }
 
     public User get(int id) {
+        logger.info("get user {}", id);
         return checkNotFoundWithId(repository.get(id), id);
     }
 
     public User create(User user) {
+        logger.info("create user {}", user);
         Assert.notNull(user, "user must not be null");
         return repository.save(user);
     }
 
     public void update(User user, int id) {
+        logger.info("update user {} id {}", user, id);
         Assert.notNull(user, "user must not be null");
-        checkNotFoundWithId(repository.save(user), user.id());
+        checkNotFoundWithId(repository.save(user), id);
     }
 
     public void delete(int id) {
+        logger.info("delete user {}", id);
         checkNotFoundWithId(repository.delete(id), id);
     }
 }
