6a9cd0536f16f53d97c1a57f277ec8313a40f7c7
Leon Galushko <leon-galushko@mail.ru>
2020-08-07 12:25:01 +0300

    controller to rest

---
+++ src/main/java/ru/graduation/web/UserController.java
@@ -0,0 +1,70 @@
+package ru.graduation.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.graduation.model.User;
+import ru.graduation.repository.UserRepository;
+
+import java.net.URI;
+import java.util.List;
+
+import static ru.graduation.util.ValidationUtil.*;
+
+@RestController
+@RequestMapping(value = UserController.REST_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+    static final String REST_URL = "/rest/admin/users";
+    private final UserRepository repository;
+    private final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    public UserController(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        logger.info("getAll users");
+        return repository.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User get(@PathVariable int id) {
+        logger.info("get user {}", id);
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> createWithLocation(@RequestBody User user) {
+        logger.info("create user {}", user);
+        checkNew(user);
+        Assert.notNull(user, "user must not be null");
+        User created = repository.save(user);
+        URI uriOfNewResource = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(created.getId()).toUri();
+        return ResponseEntity.created(uriOfNewResource).body(created);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void update(@RequestBody User user, @PathVariable int id) {
+        logger.info("update user {} id {}", user, id);
+        assureIdConsistent(user, id);
+        Assert.notNull(user, "user must not be null");
+        checkNotFoundWithId(repository.save(user), user.id());
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id) {
+        logger.info("delete user {}", id);
+        repository.delete(id);
+//        checkNotFoundWithId(repository.delete(id), id);
+    }
+}
