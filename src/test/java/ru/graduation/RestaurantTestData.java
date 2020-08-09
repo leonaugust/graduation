6fe443ea21b18903a652cbbd77f79f29f11b26a8
Leon Galushko <leon-galushko@mail.ru>
2020-08-09 13:28:13 +0300

    tests for restaurant

---
+++ src/test/java/ru/graduation/RestaurantTestData.java
@@ -0,0 +1,32 @@
+package ru.graduation;
+
+import ru.graduation.model.Restaurant;
+
+import java.util.List;
+
+import static ru.graduation.model.AbstractBaseEntity.START_SEQ;
+
+public class RestaurantTestData {
+    public static TestMatcher<Restaurant> RESTAURANT_MATCHER =
+            TestMatcher.usingFieldsComparator(Restaurant.class, "menu");
+
+    public static final int NOT_FOUND = 10;
+    public static final int RATATOUILLE_ID = START_SEQ + 2;
+    public static final int PIZZA_PLANET_ID = START_SEQ + 3;
+    public static final int KRUSTY_KRAB_ID = START_SEQ + 4;
+
+    public static final Restaurant RATATOUILLE = new Restaurant(RATATOUILLE_ID, "La Ratatouille", "Disney universe", "www.la-ratatouille.com");
+    public static final Restaurant PIZZA_PLANET = new Restaurant(PIZZA_PLANET_ID, "Pizza Planet", "Pixar universe", "www.pizza-planet.com");
+    public static final Restaurant KRUSTY_KRAB = new Restaurant(KRUSTY_KRAB_ID, "The Krusty Krab ", "831 Bottom Feeder Lane", "www.krusty-krab.com");
+
+
+    public static final List<Restaurant> RESTAURANTS = List.of(RATATOUILLE, PIZZA_PLANET, KRUSTY_KRAB);
+
+    public static Restaurant getNew() {
+        return new Restaurant(null, "Created restaurant", "Created address", "Created website");
+    }
+
+    public static Restaurant getUpdated() {
+        return new Restaurant(KRUSTY_KRAB_ID, "Updated restaurant", "Updated address", "Updated website");
+    }
+}
---
+++ src/test/java/ru/graduation/web/RestaurantControllerTest.java
@@ -0,0 +1,76 @@
+package ru.graduation.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import ru.graduation.model.Restaurant;
+import ru.graduation.util.exception.NotFoundException;
+import ru.graduation.web.json.JsonUtil;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.graduation.RestaurantTestData.*;
+import static ru.graduation.TestUtil.readFromJson;
+
+public class RestaurantControllerTest extends AbstractControllerTest {
+    private static final String REST_URL = RestaurantController.REST_URL + '/';
+
+    @Autowired
+    private RestaurantController controller;
+
+    @Test
+    void get() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL + KRUSTY_KRAB_ID))
+                .andExpect(status().isOk())
+                .andDo(print())
+                // https://jira.spring.io/browse/SPR-14472
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(RESTAURANT_MATCHER.contentJson(KRUSTY_KRAB));
+    }
+
+    @Test
+    void delete() throws Exception {
+        perform(MockMvcRequestBuilders.delete(REST_URL + KRUSTY_KRAB_ID))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+        assertThrows(NotFoundException.class, () -> controller.get(KRUSTY_KRAB_ID));
+    }
+
+    @Test
+    void update() throws Exception {
+        Restaurant updated = getUpdated();
+        perform(MockMvcRequestBuilders.put(REST_URL + KRUSTY_KRAB_ID)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(updated)))
+                .andExpect(status().isNoContent());
+
+        RESTAURANT_MATCHER.assertMatch(controller.get(KRUSTY_KRAB_ID), updated);
+    }
+
+    @Test
+    void createWithLocation() throws Exception {
+        Restaurant newRestaurant = getNew();
+        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(JsonUtil.writeValue(newRestaurant)))
+                .andExpect(status().isCreated());
+
+        Restaurant created = readFromJson(action, Restaurant.class);
+        int newId = created.id();
+        newRestaurant.setId(newId);
+        RESTAURANT_MATCHER.assertMatch(created, newRestaurant);
+        RESTAURANT_MATCHER.assertMatch(controller.get(newId), newRestaurant);
+    }
+
+    @Test
+    void getAll() throws Exception {
+        perform(MockMvcRequestBuilders.get(REST_URL))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(RESTAURANT_MATCHER.contentJson(RESTAURANTS));
+    }
+}
