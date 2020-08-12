3771535e28471a04d08d8ab27bad4efc93385f4e
Leon Galushko <leon-galushko@mail.ru>
2020-08-12 12:33:45 +0300

    add authorization

--- src/main/java/ru/graduation/web/RootController.java
+++ src/main/java/ru/graduation/web/RootController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RootController {
@@ -27,6 +30,13 @@ public class RootController {
         return "users";
     }
 
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.parseInt(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        return "redirect:restaurants";
+    }
+
     @GetMapping("/restaurants")
     public String getRestaurants(Model model) {
         model.addAttribute("restaurants", restaurantController.getAll());
@@ -38,11 +48,4 @@ public class RootController {
         model.addAttribute("meals", mealController.getAll());
         return "meals";
     }
-
-//    @PostMapping("/users")
-//    public String setUser(HttpServletRequest request) {
-//        int userId = Integer.parseInt(request.getParameter("userId"));
-//        SecurityUtil.setAuthUserId(userId);
-//        return "redirect:meals";
-//    }
 }
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -7,15 +7,15 @@
 <body>
 <jsp:include page="fragments/bodyHeader.jsp"/>
 <br>
-<h3>Hello World</h3>
-<%--<section>--%>
-<%--    <form method="post" action="users"><spring:message code="app.login"/>: <select name="userId">--%>
-<%--        <option value="100000" selected>User</option>--%>
-<%--        <option value="100001">Admin</option>--%>
-<%--    </select>--%>
-<%--        <button type="submit"><spring:message code="common.select"/></button>--%>
-<%--    </form>--%>
-<%--</section>--%>
-<%--<jsp:include page="fragments/footer.jsp"/>--%>
+<section>
+    <form method="post" action="users">
+        Login as: <select name="userId">
+        <option value="100000" selected>User</option>
+        <option value="100001">Admin</option>
+    </select>
+        <button type="submit">Select</button>
+    </form>
+</section>
+<jsp:include page="fragments/footer.jsp"/>
 </body>
 </html>
