74d48081e64f57a6f5ba168ab6a3be6241ab216a
Leon Galushko <leon-galushko@mail.ru>
2020-08-16 11:23:02 +0300

    add userTo

---
+++ src/main/java/ru/graduation/to/UserTo.java
@@ -0,0 +1,63 @@
+package ru.graduation.to;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+public class UserTo extends BaseTo implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String name;
+
+    @NotBlank
+    @Size(min = 2, max = 100)
+    private String login;
+
+    @NotBlank
+    @Size(min = 5, max = 100, message = "length must be between 5 and 100 characters")
+    private String password;
+
+    public UserTo() {
+    }
+
+    public UserTo(Integer id, String name, String login, String password) {
+        super(id);
+        this.name = name;
+        this.login = login;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserTo{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+}
